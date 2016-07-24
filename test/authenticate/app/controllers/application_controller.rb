f078800e155527ca517ec48fed3aacbcac93447f
Daniel Staudigel <dstaudigel@gmail.com>
2016-07-24 14:17:49 -0700

    Trivial users model, and connect things the way that Detour expects.

--- test/authenticate/app/controllers/application_controller.rb
+++ test/authenticate/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, only: :route
+  
+  def route
+    Rails.logger.info "User ID: #{ current_user.id }"
+
+    response.headers['X-Detour-Continue'] = 'true'
+    response.headers['X-Detour-Add-Headers'] = "X-Detour-User-Id=#{ current_user.id }"
+    render :text => 'OK'
+  end
 end
+
+
